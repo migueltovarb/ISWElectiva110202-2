8adda910e615cf99dd8cb3b9425554ae40efb3f7
SweetArctik <juan.roseroc@campusucc.edu.co>
2025-05-11 12:08:22 -0500

    Especificacion del prefix en main.py + tags en produccion + logs de start y stop del servidor

--- backend-verkada/app/main.py
+++ backend-verkada/app/main.py
@@ -1,9 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import user, visitor, acces_control, visit
 
-app = FastAPI()
+app = FastAPI(
+    title="Sistema de Control de Acceso",
+    version="1.0.0",
+    description="API para gestión de accesos, usuarios y visitantes."
+)
 
-app.include_router(user.router)
-app.include_router(visitor.router)
-app.include_router(acces_control.router)
-app.include_router(visit.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(user.router, prefix="/api/users", tags=["Users"])
+app.include_router(visitor.router, prefix="/api/visitors", tags=["Visitors"])
+app.include_router(acces_control.router, prefix="/api/access", tags=["Access Control"])
+app.include_router(visit.router, prefix="/api/visits", tags=["Visits"])
+
+@app.on_event("startup")
+async def startup_event():
+    print("API Iniciada Correctamente")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("API Finalizada Correctamente")
