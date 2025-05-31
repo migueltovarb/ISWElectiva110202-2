13ab3b45d0cab6d49f2158b2f2e22fe464b0bd26
Juanjose190 <juanbmusic8@gmail.com>
2025-05-30 23:13:18 -0500

    nuevo front, nuevas pruebas

--- frontend-verkada/vite.config.js
+++ frontend-verkada/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000  // 👈 Esto es lo que hace que npm run dev corra en localhost:3000
+  },
   test: {
     environment: 'jsdom',
     globals: true,
